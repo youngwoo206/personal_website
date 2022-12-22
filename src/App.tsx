237856d0bb8da0db4eb8ee95c3a0bf4d3bd9d3de
Youngwoo <youngwoo206@gmail.com>
2022-12-21 22:15:38 -0500

    collapsable navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 
+import Navigation from './components/Navigation';
+
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 
@@ -9,6 +11,7 @@ function App() {
 
   return (
     <Router>
+      <Navigation/>
       <Routes>
         <Route index element={<Home />} />
         <Route path="projects" element={<Projects/>}/>
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Navigation() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Resume</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
