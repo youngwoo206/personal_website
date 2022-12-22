4777e2dd0205a1dbb1df33edb42acabb4c2a6fdb
Youngwoo <youngwoo206@gmail.com>
2022-12-21 22:57:59 -0500

    working on expand and collapse

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,33 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useState, useEffect } from 'react'
+import '../styles/Navbar.css'
 
 function Navigation() {
+
+  const [show, setShow] = useState(true)
+
+  const controlNavbar = () => {
+    if (window.scrollY > 100) {
+      setShow(false)
+    }
+    else {
+      setShow(true)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar)
+
+    return () => {
+      window.removeEventListener('scroll', controlNavbar)
+    }
+  },[])
+
+
   return (
-    <Navbar collapseOnSelect sticky="top" expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -17,7 +40,7 @@ function Navigation() {
           </Nav>
         </Navbar.Collapse>
       </Container>
-    </Navbar>
+      </Navbar>
   );
 }
 
