b4e751d41485ceffb85fadb9273228f84eb8aaca
Youngwoo <youngwoo206@gmail.com>
2022-12-21 22:06:09 -0500

    setup bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 
 import Home from './pages/Home'
