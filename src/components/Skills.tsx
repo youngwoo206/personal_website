c7b71cd752323faf51e8c941410a002948d9fb38
Youngwoo <youngwoo206@gmail.com>
2022-12-23 00:48:18 -0500

    updated skill tabs

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,30 +1,62 @@
-import React, {useState} from 'react'
-import '../styles/TextSphere.css'
-import "../styles/Skills.css"
-import TextSphere from './TextSphere'
+import React, { useState } from "react";
+import "../styles/TextSphere.css";
+import "../styles/Skills.css";
+import TextSphere from "./TextSphere";
 
 function Skills() {
+  const [tab, setTab] = useState(1);
 
-    const [tab, setTab] = useState(1)
-
-    const toggleTab = () => {
-
-    }
+  const toggleTab = (index: number) => {
+    setTab(index);
+  };
 
   return (
-    <div className='skills'>
-        <h1>My Skills:</h1>
-        <TextSphere />
-          
-        <div className='skill-container'>
-              <div className='bloc-tabs'>
-                  <div className='tabs active-tabs' onClick={toggleTab}>Tab 1</div>
-                  <div className='tabs' onClick={toggleTab}>Tab 2</div>
-                  <div className='tabs' onClick={toggleTab}>Tab 3</div>
-            </div>
+    <div className="skills">
+      <h1>My Skills:</h1>
+
+      <TextSphere />
+
+      <div className="skill-container">
+        <div className="bloc-tabs">
+          <div
+            className={tab === 1 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(1)}
+          >
+            Web Dev
+          </div>
+          <div
+            className={tab === 2 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(2)}
+          >
+            Software
+          </div>
+          <div
+            className={tab === 3 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(3)}
+          >
+            Engineering
+          </div>
         </div>
+
+        <div className="content-tabs">
+          <div className={tab === 1 ? "content  active-content" : "content"}>
+            <h2>Web Development</h2>
+            <p>web dev</p>
+          </div>
+
+          <div className={tab === 2 ? "content  active-content" : "content"}>
+            <h2>Software Development</h2>
+            <p>software</p>
+          </div>
+
+          <div className={tab === 3 ? "content  active-content" : "content"}>
+            <h2>Engineering</h2>
+            <p>eng stuff</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
