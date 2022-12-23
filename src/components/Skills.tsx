9cd6123ed281a8dab8f6ffec85bfe0d34f91ccde
Youngwoo <youngwoo206@gmail.com>
2022-12-23 00:16:29 -0500

    adding skill tabs

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/TextSphere.css'
+import "../styles/Skills.css"
 import TextSphere from './TextSphere'
 
 function Skills() {
 
+    const [tab, setTab] = useState(1)
+
+    const toggleTab = () => {
+
+    }
+
   return (
-    <div>
-        <TextSphere/>
+    <div className='skills'>
+        <h1>My Skills:</h1>
+        <TextSphere />
+          
+        <div className='skill-container'>
+              <div className='bloc-tabs'>
+                  <div className='tabs active-tabs' onClick={toggleTab}>Tab 1</div>
+                  <div className='tabs' onClick={toggleTab}>Tab 2</div>
+                  <div className='tabs' onClick={toggleTab}>Tab 3</div>
+            </div>
+        </div>
     </div>
   )
 }
