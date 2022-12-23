da05d2b19687ddadf4c44c36b1b62a44c95feb04
Youngwoo <youngwoo206@gmail.com>
2022-12-22 23:52:34 -0500

    center content

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,26 +4,26 @@ import Typical from 'react-typical'
 function Intro() {
   return (
     <div className="intro">
-    <h1>Hi, I'm Youngwoo</h1>
-    <p>
-      <Typical
-        loop={Infinity}
-        wrapper='p'
-        steps={[
-            'software developer',
-            2000,
-            'engineering student',
-            2000,
-            'lifelong learner',
-            2000,
-            'lego enthusiast',
-            2000,
-            'raptors fan',
-            2000,
-        ]}
-      /> 
-    </p>
-  </div>
+        <h1>Hi, I'm Youngwoo</h1>
+        <p>
+        <Typical
+            loop={Infinity}
+            wrapper='p'
+            steps={[
+                'software developer',
+                2000,
+                'engineering student',
+                2000,
+                'lifelong learner',
+                2000,
+                'lego enthusiast',
+                2000,
+                'raptors fan',
+                2000,
+            ]}
+        /> 
+        </p>
+    </div>
   )
 }
 
