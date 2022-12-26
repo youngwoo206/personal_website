ffb3d3ab7ac30d6ed517bada031b4a913b0ae8fb
Youngwoo <youngwoo206@gmail.com>
2022-12-25 20:04:12 -0500

    revert snap scroll

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function AboutMe() {
   return (
-    <div>
+    <div className="about-me">
       <h1>About Me</h1>
       <p>
         I'm a 3rd year chemical engineering student from the University of
