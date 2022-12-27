76d9a864d9e3bf45b08c2d058640621b8f3f635b
Youngwoo <youngwoo206@gmail.com>
2022-12-26 21:19:04 -0500

    responsive skills section grid

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -14,44 +14,46 @@ function Skills() {
     <div className="skills">
       <h1>My Skills:</h1>
 
-      <TextSphere />
+      <div className="skills-grid">
+        <TextSphere />
 
-      <div className="skill-container">
-        <div className="bloc-tabs">
-          <div
-            className={tab === 1 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(1)}
-          >
-            Web Dev
-          </div>
-          <div
-            className={tab === 2 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(2)}
-          >
-            Software
-          </div>
-          <div
-            className={tab === 3 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(3)}
-          >
-            Engineering
-          </div>
-        </div>
-
-        <div className="content-tabs">
-          <div className={tab === 1 ? "content  active-content" : "content"}>
-            <h2>Web Development</h2>
-            <p>web dev</p>
+        <div className="skill-container">
+          <div className="bloc-tabs">
+            <div
+              className={tab === 1 ? "tabs active-tabs" : "tabs"}
+              onClick={() => toggleTab(1)}
+            >
+              Web Dev
+            </div>
+            <div
+              className={tab === 2 ? "tabs active-tabs" : "tabs"}
+              onClick={() => toggleTab(2)}
+            >
+              Software
+            </div>
+            <div
+              className={tab === 3 ? "tabs active-tabs" : "tabs"}
+              onClick={() => toggleTab(3)}
+            >
+              Engineering
+            </div>
           </div>
 
-          <div className={tab === 2 ? "content  active-content" : "content"}>
-            <h2>Software Development</h2>
-            <p>software</p>
-          </div>
+          <div className="content-tabs">
+            <div className={tab === 1 ? "content  active-content" : "content"}>
+              <h2>Web Development</h2>
+              <p>web dev</p>
+            </div>
 
-          <div className={tab === 3 ? "content  active-content" : "content"}>
-            <h2>Engineering</h2>
-            <p>eng stuff</p>
+            <div className={tab === 2 ? "content  active-content" : "content"}>
+              <h2>Software Development</h2>
+              <p>software</p>
+            </div>
+
+            <div className={tab === 3 ? "content  active-content" : "content"}>
+              <h2>Engineering</h2>
+              <p>eng stuff</p>
+            </div>
           </div>
         </div>
       </div>
