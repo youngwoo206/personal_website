b623ea709ea2574da67b3b90e90c0cb8f2d01169
Youngwoo <youngwoo206@gmail.com>
2022-12-26 21:36:59 -0500

    styling skills tabs

--- src/components/TextSphere.tsx
+++ src/components/TextSphere.tsx
@@ -1,48 +1,47 @@
-import React, { useEffect } from 'react'
-import TagCloud from 'TagCloud'
+import React, { useEffect } from "react";
+import TagCloud from "TagCloud";
 
 function TextSphere() {
+  useEffect(() => {
+    return () => {
+      const container = ".tagcloud";
+      const text = [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "TypeScript",
+        "SASS",
+        "Tailwind CSS",
+        "Bootstrap",
+        "Python",
+        "Data Structures",
+        "Algorithms",
+        "React",
+        "Node.js",
+        "React Native",
+        "Firebase",
+        "SQL",
+        "VBA",
+        "Java",
+        "Redux",
+        "Git",
+      ];
+      const options = {
+        radius: 280,
+        maxSpeed: "slow",
+        initSpeed: "none",
+        keep: false,
+      };
 
-    useEffect(() => {
-        return () => {
-            const container = ".tagcloud"
-            const text = [
-                "HTML",
-                "CSS",
-                "JavaScript",
-                "TypeScript",
-                "SASS",
-                "Tailwind CSS",
-                "Bootstrap",
-                "Python",
-                "Data Structures",
-                "Algorithms",
-                "React",
-                "Node.js",
-                "React Native",
-                "Firebase",
-                "SQL",
-                "VBA",
-                "Java",
-                "Redux",
-                "Git",
-            ]
-            const options = {
-                radius: 300,
-                maxSpeed: "slow",
-                initSpeed: "none",
-                keep: true,
-            }
-
-            TagCloud(container, text, options)
-      }
-    },[])
+      TagCloud(container, text, options);
+    };
+  }, []);
 
   return (
-    <div className='text-sphere'>
-        <span className='tagcloud'></span>
+    <div className="text-sphere">
+      <span className="tagcloud"></span>
     </div>
-  )
+  );
 }
 
-export default TextSphere
+export default TextSphere;
