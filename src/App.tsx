34367fb15a6ef15a922a5e1d4df35ba1c5b29072
Youngwoo <youngwoo206@gmail.com>
2023-01-09 01:29:17 -0500

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 
-import Navigation from './components/Navigation';
+import Navigation from "./components/Navigation";
 
-import Home from './pages/Home'
-import Projects from './pages/Projects'
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 function App() {
-
   return (
     <Router>
-      <Navigation/>
+      <Navigation />
       <Routes>
         <Route index element={<Home />} />
-        <Route path="projects" element={<Projects/>}/>
+        <Route path="projects" element={<Projects />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 import "../styles/Footer.css";
+import { Github, Linkedin, EnvelopeAt } from "react-bootstrap-icons";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-body">
+      <a
+        className="footer-icon"
+        href="https://github.com/youngwoo206"
+        target="_blank"
+      >
+        <Github size={40} />
+      </a>
+      <a
+        className="footer-icon"
+        href="https://www.linkedin.com/in/youngwoo-lee-123b63198/"
+        target="_blank"
+      >
+        <Linkedin size={40} />
+      </a>
+      <a
+        className="footer-icon"
+        href="mailto:youngwoo206@gmail.com"
+        target="_blank"
+        color="white"
+      >
+        <EnvelopeAt size={40} />
+      </a>
+    </div>
+  );
 }
 
 export default Footer;
