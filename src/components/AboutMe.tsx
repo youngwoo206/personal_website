b5ef068ecca3dfb0f2a536c7c2e6b0e46f58e7e8
Youngwoo <youngwoo206@gmail.com>
2023-01-11 23:56:11 -0500

    about me content done

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,14 +4,31 @@ import "../styles/AboutMe.css";
 function AboutMe() {
   return (
     <div className="about-me">
-      <h1>About Me</h1>
-      <div className="about-me-grid">
+      <h1>
+        <span className="accent">~</span>About Me
+        <span className="accent">~</span>
+      </h1>
+      <div className="profile-pic"></div>
+      <div className="about-me-text">
         <p>
-          Hi, I'm Youngwoo! I'm a 3rd year chemical engineering student from the
-          University of Waterloo. I'm passionate about software development,
-          including web development, and all things engineering!
+          I'm a 3rd year chemical engineering student from the University of
+          Waterloo with a passion for software development. I'm a creative
+          thinker who loves to come up with innovative solutions to complex
+          problems. I'm also a versatile worker who thrives in both independent
+          and team environments.
         </p>
-        <p></p>
+        <p>
+          As for my interests, I generally find myself drawn to all things
+          engineering. Anything from programming to soldering, planes, cars,
+          robotics and more all catch my attention. Aside from that I like to
+          play basketball, hockey, and strategy games. I also have 2 pet dogs
+          and can be bribed to do pretty much anything in exchange for some
+          McDonald's.
+        </p>
+        <p>
+          Please feel free to contact me anytime! You can find my contact
+          information <span className="accent">here</span>
+        </p>
       </div>
     </div>
   );
